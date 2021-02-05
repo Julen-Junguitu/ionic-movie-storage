9f83b14102e1208538d697c31d09d36a2a09fbd1
Julen Junguitu <julenjunguitu@outlook.es>
2021-02-05 23:36:09 +0100

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,36 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    path: 'home', loadChildren: () => import('./home/home.module').then(
+      m => m.HomePageModule)
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: 'details/:id', loadChildren:
+      './details/details.module#DetailsPageModule'
+  },
+  {
+    path: 'create', loadChildren:
+      './create/create.module#CreatePageModule'
+  },
+  {
+    path: 'edit/:id', loadChildren: './edit/edit.module#EditPageModule'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'create',
+    loadChildren: () => import('./create/create.module').then( m => m.CreatePageModule)
+  },
+  {
+    path: 'details',
+    loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
+  },
 ];
+
 
 @NgModule({
   imports: [
@@ -19,4 +39,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
